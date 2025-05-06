4f1a06a43b6eb1d5351c8336f823d01f7616e90c
Richmontoya92 <r.montoya.alsina@gmail.com>
2025-05-06 20:51:04 +0000

    cambios casi listos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl);
+$(document).ready(function() {
+  $('#enviarButton').attr('title', '¡VIAJA CON NOSOTROS!');
+});
+
+$(document).ready(function() {
+  // Añadir el tooltip con jQuery
+  $('#enviarButton').attr('title', '¡VIAJA CON NOSOTROS!');
+  
+  // Mostrar la alerta al hacer clic en el botón
+  $('#enviarButton').click(function(e) {
+    e.preventDefault(); // Evitar que el formulario se envíe si es un formulario
+    alert('Nos comunicaremos pronto');
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".card-img-top");
+
+  images.forEach(image => {
+    image.addEventListener("click", function () {
+      const cardBody = this.parentElement.querySelector(".card-body");
+      if (cardBody) {
+        cardBody.classList.toggle("d-none");
+      }
     });
   });
+});
